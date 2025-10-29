e669c321693ed9a6b4c84cbd3554e59dbb58b12e
NOname <123456@123.com>
2025-10-29 22:10:12 +0800

    a,b,c求最大值

--- 10.28/10.28/源.c
+++ 10.28/10.28/源.c
@@ -2,15 +2,48 @@
 
 #include <stdio.h>
 
+//int main()
+//{//------------------ 1+2+3+..+99+100的和
+//	int i = 0;
+//	int sum = 0;
+//	for (i = 0; i <= 100; i++)
+//	{
+//		sum += i;
+//	}
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
 int main()
-{
-	int i = 0;
-	int sum = 0;
-	for (i = 0; i <= 100; i++)
-	{
-		sum += i;
-	}
-	printf("%d\n", sum);
+{//--------------------求a,b,c中的最大数
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	printf("输入三个数字a,b,c：");
+	scanf("%d,%d,%d", &a, &b, &c);
+	//if (a > b)
+	//{
+	//	if (a > c)
+	//		printf("最大数是：%d\n", a);
+	//	else
+	//		printf("最大数是：%d\n", c);
+	//}
+	//else
+	//{
+	//	if (b > c)
+	//		printf("最大数是：%d\n", b);
+	//	else
+	//		printf("最大数是：%d\n", c);
+	//}
+            //---------解法二  
+	int max = a;
+	if (max < b)
+		max = b;
+	if (max < c)
+		max = c;
+	printf("最大数是：%d\n", max);
+
 
 	return 0;
 }
